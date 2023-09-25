b3d3fa325c9200d46b7238f76d1cce63e96515fe
Paul <70605634+ptacode@users.noreply.github.com>
2023-09-25 14:53:54 -0600

    First React

---
+++ React/reactbootstarp/src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Card(){
+    return(
+        <div>
+            Esto es una tarjeta
+            <h1>Hola Mundo</h1>
+            <h2>Paul Torres Aguillon</h2>
+            <p>null</p>
+        </div>
+    )
+}
