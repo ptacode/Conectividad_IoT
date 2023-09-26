a182ab45ec73412e22459494cfbedafa3e69d46e
Paul <70605634+ptacode@users.noreply.github.com>
2023-09-26 20:57:12 +0000

    Remove logo and commented "Card"

--- React/reactbootstarp/src/App.js
+++ React/reactbootstarp/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import Card from './components/Card';
+//import Card from './components/Card';
 import Cards from './components/Cards';
 
 function App() {
